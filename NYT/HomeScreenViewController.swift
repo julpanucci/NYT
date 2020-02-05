1a075790a269b6a4832fdbb5b443021f9bd41f53
Julian Panucci <julpanucci@gmail.com>
2020-02-05 15:31:45 -0500

    Added searchField

--- NYT/HomeScreenViewController.swift
+++ NYT/HomeScreenViewController.swift
@@ -15,7 +15,7 @@ class HomeScreenViewController: UIViewController, HomeScreenViewProtocol {
     
     var articles = [Article]()
     var page = 0
-    var searchText: String?
+    var searchText: String = ""
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -31,13 +31,27 @@ class HomeScreenViewController: UIViewController, HomeScreenViewProtocol {
     
     lazy var headerView: UIView = {
         let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 100))
-        view.backgroundColor = .white
+        view.backgroundColor = self.view.backgroundColor
+        
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.customFont(.chomsky, type: .regular, size: 30)
+        titleLabel.text = "New York Times"
+        
         
         view.addSubview(searchField)
+        view.addSubview(titleLabel)
         NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            titleLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - 32),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            titleLabel.heightAnchor.constraint(equalToConstant: 40),
+            
             self.searchField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            self.searchField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16),
-            self.searchField.heightAnchor.constraint(equalToConstant: 20),
+            self.searchField.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8),
+            self.searchField.heightAnchor.constraint(equalToConstant: 36),
+            self.searchField.widthAnchor.constraint(equalToConstant: view.bounds.width - 32),
         ])
         return view
     }()
@@ -52,9 +66,10 @@ class HomeScreenViewController: UIViewController, HomeScreenViewProtocol {
         searchField.clearButtonMode = .always
         searchField.autocorrectionType = .no
         searchField.autocapitalizationType = .none
-        searchField.placeholder = "Search"
+        searchField.placeholder = "Search for an article"
         searchField.returnKeyType = .search
         searchField.translatesAutoresizingMaskIntoConstraints = false
+        searchField.delegate = self
         
         let searchIcon = UIImageView(image: UIImage(named: "search_icon"))
         searchIcon.tintColor = .black
@@ -67,19 +82,25 @@ class HomeScreenViewController: UIViewController, HomeScreenViewProtocol {
     lazy var loadingView: LoadingView = {
         let view = LoadingView(frame: self.view.frame)
         view.descriptionText = "Searching for articles..."
+        view.isHidden = true
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = UIColor(red: 0.01, green: 0.51, blue: 0.56, alpha: 1.00)
+        
+        self.setupNavigationController()
         
         self.setConstraints()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        self.presenter?.getArticles(searchText: "election", page: 0)
+    func setupNavigationController() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = .white
     }
     
     func setConstraints() {
@@ -143,11 +164,19 @@ extension HomeScreenViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.row == self.articles.count - 1 {
             page += 1
-            self.presenter?.getArticles(searchText: "election", page: page)
+            self.presenter?.getArticles(searchText: self.searchText, page: page)
         }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
+    }
+}
+
+extension HomeScreenViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.searchText = textField.text ?? ""
+        self.presenter?.getArticles(searchText: searchText, page: 0)
+        return true
     }
 }
---
+++ NYT/UIFont+extensions.swift
@@ -0,0 +1,35 @@
+//
+//  UIFont+extensions.swift
+//  NYT
+//
+//  Created by Julian Panucci on 2/5/20.
+//  Copyright © 2020 Panucci. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIFont {
+
+    public enum `Type`: String {
+        case semibold = "-SemiBold"
+        case regular = "-Regular"
+        case light = "-Light"
+        case extraBold = "-ExtraBold"
+        case bold = "-Bold"
+    }
+
+    public enum CustomFont: String {
+        case chomsky = "Chomsky"
+    }
+
+    static func customFont(_ font: CustomFont, type: Type = .regular, size: CGFloat = UIFont.systemFontSize) -> UIFont {
+        if let font = UIFont(name: "\(font.rawValue)", size: size) {
+            return font
+        } else {
+            print("\nCould not find font. Make sure it is added to your Info.plist!\n")
+            return UIFont.systemFont(ofSize: size)
+        }
+    }
+
+}
