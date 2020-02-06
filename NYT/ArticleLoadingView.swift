4af6f7f84e80ea61d50e5f8736de3a10ac9a01e8
Julian Panucci <panuccij2@upmc.edu>
2020-02-06 10:22:38 -0500

    Update ArticleLoadingView

--- NYT/ArticleLoadingView.swift
+++ NYT/ArticleLoadingView.swift
@@ -27,14 +27,6 @@ class ArticleLoadingView: UIView {
         return textView
     }()
     
-    lazy var textView3: UITextView = {
-        let textView = UITextView()
-        textView.isSkeletonable = true
-        textView.backgroundColor = .clear
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        return textView
-    }()
-    
     lazy var placeHolderView1: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -48,25 +40,32 @@ class ArticleLoadingView: UIView {
         view.isSkeletonable = true
         return view
     }()
-    
-    lazy var placeHolderView3: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.isSkeletonable = true
-        return view
+
+    lazy var label1: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isSkeletonable = true
+        return label
+    }()
+
+    lazy var label2: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isSkeletonable = true
+        return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.isSkeletonable = true
-        
+
+        self.addSubview(label1)
+        self.addSubview(label2)
         self.addSubview(placeHolderView1)
         self.addSubview(textView1)
         self.addSubview(textView2)
         self.addSubview(placeHolderView2)
-        self.addSubview(textView3)
-        self.addSubview(placeHolderView3)
         self.setConstraints()
     }
     
@@ -77,35 +76,40 @@ class ArticleLoadingView: UIView {
     func setConstraints() {
         
         NSLayoutConstraint.activate([
-            placeHolderView1.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
-            placeHolderView1.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
-            placeHolderView1.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
-            placeHolderView1.heightAnchor.constraint(equalToConstant: 100),
-            
-            textView1.topAnchor.constraint(equalTo: placeHolderView1.bottomAnchor, constant: 8),
+            label1.topAnchor.constraint(equalTo: self.topAnchor, constant: 16),
+            label1.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            label1.widthAnchor.constraint(equalToConstant: 200),
+            label1.heightAnchor.constraint(equalToConstant: 30),
+
+            label2.topAnchor.constraint(equalTo: label1.topAnchor, constant: 64),
+            label2.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            label2.widthAnchor.constraint(equalToConstant: 200),
+            label2.heightAnchor.constraint(equalToConstant: 30),
+
+            textView1.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 8),
             textView1.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
             textView1.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
             textView1.heightAnchor.constraint(equalToConstant: 100),
+
+            textView2.topAnchor.constraint(equalTo: textView1.bottomAnchor, constant: 8),
+            textView2.leadingAnchor.constraint(equalTo: textView1.leadingAnchor, constant: 0),
+            textView2.trailingAnchor.constraint(equalTo: textView1.trailingAnchor, constant: 0),
+            textView2.heightAnchor.constraint(equalToConstant: 100),
+
+            placeHolderView1.topAnchor.constraint(equalTo: textView2.bottomAnchor, constant: 8),
+            placeHolderView1.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            placeHolderView1.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            placeHolderView1.heightAnchor.constraint(equalToConstant: 250),
             
-            placeHolderView2.topAnchor.constraint(equalTo: textView1.bottomAnchor, constant: 8),
+            placeHolderView2.topAnchor.constraint(equalTo: placeHolderView1.bottomAnchor, constant: 8),
             placeHolderView2.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
             placeHolderView2.heightAnchor.constraint(equalToConstant: 100),
             placeHolderView2.widthAnchor.constraint(equalToConstant: 150),
             
-            textView2.topAnchor.constraint(equalTo: textView1.bottomAnchor, constant: 8),
+            textView2.topAnchor.constraint(equalTo: placeHolderView1.bottomAnchor, constant: 8),
             textView2.leadingAnchor.constraint(equalTo: placeHolderView2.trailingAnchor, constant: 8),
             textView2.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
             textView2.heightAnchor.constraint(equalToConstant: 100),
-            
-            placeHolderView3.topAnchor.constraint(equalTo: placeHolderView2.bottomAnchor, constant: 8),
-            placeHolderView3.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
-            placeHolderView3.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
-            placeHolderView3.heightAnchor.constraint(equalToConstant: 100),
-            
-            textView3.topAnchor.constraint(equalTo: placeHolderView3.bottomAnchor, constant: 8),
-            textView3.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
-            textView3.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
-            textView3.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
     
